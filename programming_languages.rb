ce817aa906e787bed5e8b90bb5b712bbcd15e750
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 21:01:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,6 @@
 def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |style, language|
+  hash = {}
+  hash.each do |style, language|
     language.each do |name, attributes|
     new_languages[name] = attributes
     new_languages[name][:style]=[]
@@ -10,51 +10,3 @@ def reformat_languages(languages)
   new_languages
 end
 
-def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |style, language|
-    language.each do |language_name, attribute|
-      if new_languages[language_name] == nil
-          new_languages[language_name] = attribute
-        end
-      new_languages[language_name][:style] = []
-      new_languages[language_name][:style] << style
-    end
-  end
-  new_languages[:javascript][:style] << :oo
-  new_languages
-end
-
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
- 
-  }
-}
-
-reformat_languages(languages)
