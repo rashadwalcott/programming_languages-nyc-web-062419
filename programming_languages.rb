5d63bde0804e5f90a3c809de98a12167ef06f353
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-23 21:04:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   hash = {}
   languages.each do |style, language|
     language.each do |name, attributes|
-    hash[name] = attributes
+    hash[name] || = attributes
     hash[name][:style]=[]
     hash[name][:style] << style
     end
